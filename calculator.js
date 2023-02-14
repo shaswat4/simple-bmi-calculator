be74196f5a2de1190c04937fd912f8cfdab5ee59
shaswat4 <shaswatagrawal01@gmail.com>
2023-02-14 17:06:38 +0530

    took input from html form and gave a simple sum to user

--- calculator.js
+++ calculator.js
@@ -1,13 +1,26 @@
 
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended : true}));
 
 app.get( "/" , function (req , res) {
     res.sendFile( __dirname +  "/index.html");
 } );
 
 app.post( "/" , function (req, res) {
-    res.send("post working correctly");
+    
+    console.log( req.body );
+
+    var n1 = Number(req.body.num1);
+    var n2 = Number(req.body.num2);
+    var ans = n1 + n2 ;
+
+    console.log(ans);
+    
+    res.send("answer is " +  ans );
+
 } );
 
 app.listen( 3000 , function () {
