bdbb63a6b19ab990645fa415da547b9cc6251dd1
shaswat4 <shaswatagrawal01@gmail.com>
2023-02-14 16:56:57 +0530

    added post route to server

--- calculator.js
+++ calculator.js
@@ -6,6 +6,10 @@ app.get( "/" , function (req , res) {
     res.sendFile( __dirname +  "/index.html");
 } );
 
+app.post( "/" , function (req, res) {
+    res.send("post working correctly");
+} );
+
 app.listen( 3000 , function () {
     console.log("listening at port 3000");
 });
